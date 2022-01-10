9a8a634d5d01224303428fc1f73a5c81e5d87f93
ashhher <asher.han.xiao@gmail.com>
2022-01-10 16:45:36 +0800

    easy

---
+++ javascript/二叉树/104.二叉树的最大深度.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=javascript
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    const dfs = (root) => {
+        if (root === null) return 0;
+        return Math.max(dfs(root.left), dfs(root.right)) + 1;
+    }
+    return dfs(root);
+};
+// @lc code=end
+
